740a3f274cb450a18874775264d67de15ccfb25b
Coutaq <7tGu9ZQn>
2021-11-08 09:58:40 +0300

    asdsad

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if='serverdata'>
+    <cpu-component :cpudata="serverdata['cpu']"></cpu-component>
+  </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CpuComponent from './components/CPU.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CpuComponent
+  },
+  data:function(){
+    return{
+      connection:null,
+      serverdata: {}
+    }
+  },
+  methods:{
+    setData(data){
+      this.serverdata = JSON.parse(data)
+      console.log(this.serverdata)
+    },
+  },
+   created() {
+     let vm = this;
+    console.log("Starting connection to WebSocket Server")
+    this.connection = new WebSocket("ws://192.168.0.105:5000/data?key=test")
+
+    this.connection.onmessage = function(event) {
+      vm.setData(event.data)
+    }
+
+    this.connection.onopen = function() {
+      // console.log(this)
+      console.log("Successfully connected to the echo websocket server...")
+    }
+
   }
+   
 }
 </script>
 
---
+++ src/components/CPU.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="cpudata">
+      <p> {{cpudata}}</p>
+      <h1>CPU LOAD: {{cpudata.total_load}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+props:['cpudata']
+}
+</script>
+
+<style>
+
+</style>
